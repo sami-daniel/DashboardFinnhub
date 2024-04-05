f26f224afdf783816c60dc7acada4a48317609ae
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 19:57:43 -0300

    Create Views and Index View at Home Controller

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174159a237527b93c0dc0bfcdf4d642928877af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a66a9e730e735c8365302a1d308156e1da91f7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
