725f979d9a0da4f7f708547e06012f6985608594
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 20:29:34 -0300

    Created FinnhubService and implemented abstraction IFinnHub

---
+++ Services/FinnhubService.cs
@@ -0,0 +1,12 @@
+﻿using ServicesAbstractions;
+
+namespace Services
+{
+    public class FinnhubService : IFinnhubService
+    {
+        public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
