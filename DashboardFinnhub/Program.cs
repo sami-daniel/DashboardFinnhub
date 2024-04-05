174159a237527b93c0dc0bfcdf4d642928877af4
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 19:55:53 -0300

    Enable defaults middleware and services

--- DashboardFinnhub/Program.cs
+++ DashboardFinnhub/Program.cs
@@ -5,9 +5,12 @@ namespace DashboardFinnhub
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllers();
 
             app.Run();
         }
