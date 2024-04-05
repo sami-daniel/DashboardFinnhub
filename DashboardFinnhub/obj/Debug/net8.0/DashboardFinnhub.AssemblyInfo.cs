735e15871b675eadbee900e71a20388ac20835a9
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 20:15:35 -0300

    added service library

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5885b8b22cf107290bdf9578a743992435b07b15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ede6dcff1ab85df716a19260da36c17a3369098")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
