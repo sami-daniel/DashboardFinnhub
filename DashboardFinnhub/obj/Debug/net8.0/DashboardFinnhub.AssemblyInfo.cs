5744df204ba51835a353d546b92db9ed8bdcab04
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 20:45:58 -0300

    Added Microsoft.Extensions.Http to services

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+735e15871b675eadbee900e71a20388ac20835a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adbd7ab765e61c6e46686c65b08478201a8f578")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
