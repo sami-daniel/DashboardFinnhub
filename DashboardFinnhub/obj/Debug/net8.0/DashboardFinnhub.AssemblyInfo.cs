7546fa4c3f1cf11955972412aa6a41b92af97787
Sami Daniel <samidaniel125@gmail.com>
2024-04-06 14:29:09 -0300

    added components structure of dir and implemented default cshtml page

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9adff262ad204ffd41b0ea97e9ef3971dd7dd818")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c91eeb5e7e1bf89fa240091e5fa5cff8c329dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
