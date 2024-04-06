41a3cc6df892da811262beab6bebb70cfc6aefc4
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 21:24:23 -0300

    Added assemblyes

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404b192656868fa2ef85dae2171e3615e8f0d3f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b498508f6221cfd3e2cbd2746ffac8ffd63834")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
