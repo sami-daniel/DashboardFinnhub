384a29dd6283a50fba5e02265668addb3664dbb9
Sami Daniel <samidaniel125@gmail.com>
2024-04-05 21:02:14 -0300

    added logical finnhubservice

--- Services/FinnhubService.cs
+++ Services/FinnhubService.cs
@@ -1,12 +1,37 @@
-﻿using ServicesAbstractions;
+﻿using Microsoft.Extensions.Configuration;
+using ServicesAbstractions;
+using System.Text.Json;
 
 namespace Services
 {
-    public class FinnhubService : IFinnhubService
+    public class FinnhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : IFinnhubService
     {
-        public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
+        readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
         {
-            throw new NotImplementedException();
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+                HttpRequestMessage httpRequestMessage = new()
+                {
+                    RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={configuration.GetSection("FinnhubAPI").GetValue<string>("AcessToken")}"),
+                    Method = HttpMethod.Post
+                };
+
+                var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+                var stream = httpResponseMessage.Content.ReadAsStream();
+                StreamReader streamReader = new(stream);
+                string response = streamReader.ReadToEnd();
+                var responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>?>(response);
+
+                if (responseDictionary == null)
+                    return null;
+                if (responseDictionary.ContainsKey("error"))
+                    return new Dictionary<string, object>()
+                    {
+                        { "error", responseDictionary["error"] }
+                    };
+                return responseDictionary;
+            }
         }
     }
 }
