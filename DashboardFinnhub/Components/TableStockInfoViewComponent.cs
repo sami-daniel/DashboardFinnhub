2c91eeb5e7e1bf89fa240091e5fa5cff8c329dce
Sami Daniel <samidaniel125@gmail.com>
2024-04-06 14:27:43 -0300

    implemented invokeasync method to return TableStockInfo view component

--- DashboardFinnhub/Components/TableStockInfoViewComponent.cs
+++ DashboardFinnhub/Components/TableStockInfoViewComponent.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DashboardFinnhub.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DashboardFinnhub.Components
 {
     [ViewComponent]
     public class TableStockInfoViewComponent : ViewComponent
     {
+        public async Task<IViewComponentResult> InvokeAsync(Stock stock)
+        {
+            return View(stock);
+        }
     }
 }
