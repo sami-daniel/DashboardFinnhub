bd0da22aa6742b6fa20e5d47d829d70f13acdc55
Sami Daniel <samidaniel125@gmail.com>
2024-04-07 09:44:52 -0300

    Implemented HomeController and logic of control

--- DashboardFinnhub/Controllers/HomeController.cs
+++ DashboardFinnhub/Controllers/HomeController.cs
@@ -9,13 +9,46 @@ namespace DashboardFinnhub.Controllers
     {
         readonly IFinnhubService _finnhubService = finnhubService;
         [Route("/")]
-        public IActionResult Index([FromQuery] string symbol = "ALL")
+        public async Task<IActionResult> Index([FromQuery] string symbol = "ALL")
         {
-            
+            symbol.ToUpper();
+            List<string> stockSymbols =
+            [
+                "AAPL", "GOOGL", "MSFT"
+            ];
+            List<Stock> stocks = [];
+            if (symbol == "ALL")
+            {
+                foreach (var item in stockSymbols)
+                {
+                    Dictionary<string, object>? response = await _finnhubService.GetStockPriceQuote(item);
+                    double? c = TryParse(response?["c"]?.ToString());
+                    double? h = TryParse(response?["h"]?.ToString());
+                    double? l = TryParse(response?["l"]?.ToString());
+                    double? o = TryParse(response?["o"]?.ToString());
+                    double? pc = TryParse(response?["pc"]?.ToString());
+                    stocks.Add(new Stock(c, h, l, o, pc));
+                }
+            }
+            else
+            {
+                foreach (var item in stockSymbols.Where(s => s.Contains("AAPL")))
+                {
+                    Dictionary<string, object>? response = await _finnhubService.GetStockPriceQuote(item);
+                    double? c = TryParse(response?["c"]?.ToString());
+                    double? h = TryParse(response?["h"]?.ToString());
+                    double? l = TryParse(response?["l"]?.ToString());
+                    double? o = TryParse(response?["o"]?.ToString());
+                    double? pc = TryParse(response?["pc"]?.ToString());
+                    stocks.Add(new Stock(c, h, l, o, pc));
+                }
+            }
+            return View();
         }
 
-        private double? TryParse(string input)
+        private static double? TryParse(string? input)
         {
+            if (input == null) return null;
             double result;
             var sucess = double.TryParse(input, CultureInfo.InvariantCulture, out result);
             return sucess ? result : null;
