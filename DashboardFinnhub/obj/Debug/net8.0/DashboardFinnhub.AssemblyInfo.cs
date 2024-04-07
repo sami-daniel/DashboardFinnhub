67f9a2aef9d8973b623d78b940e6bfb1161d3907
Sami Daniel <samidaniel125@gmail.com>
2024-04-07 09:49:25 -0300

    resolving merge conflict

--- DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
+++ DashboardFinnhub/obj/Debug/net8.0/DashboardFinnhub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8cc4a7e2bdbe00bd4ef419f8bc571090e6b415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7464e4ed1f53f164afb502cf0870f7c70ca9f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardFinnhub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
