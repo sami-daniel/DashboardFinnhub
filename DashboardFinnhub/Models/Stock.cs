bb062f7114bd7cd9e872ef942fc9c79e1c6a9d0e
Sami Daniel <samidaniel125@gmail.com>
2024-04-07 09:37:09 -0300

    resolving merge conflict in stock

--- DashboardFinnhub/Models/Stock.cs
+++ DashboardFinnhub/Models/Stock.cs
@@ -1,10 +1,6 @@
 ﻿namespace DashboardFinnhub.Models
 {
-<<<<<<< HEAD
     public class Stock(double? c, double? h, double? l, double? o, double? pc)
-=======
-    public class Stock(double c, double h, double l, double o, double pc)
->>>>>>> 4dfc53b3ca0c829b32f2611a57e66a935d105b14
     {
         public double? CurrentPrice { get; } = c;
         public double? HighPriceoftheDay{ get; } = h;
